6c07b3343e2996dfdfb9c6827e2a754dd107dcd1
emanuel2718 <eramirez2718@gmail.com>
2021-03-19 14:53:02 -0700

    push base test code

--- test_algocli.py
+++ test_algocli.py
@@ -7,8 +7,7 @@ from algocli import algocli
 
 class TestAlgocli(unittest.TestCase):
 
-    def setup(self):
-
+    def setUp(self):
         args = {
             'input': ['python', 'insertionsort'],
             'version': False,
@@ -17,7 +16,24 @@ class TestAlgocli(unittest.TestCase):
             'list_algo': False,
             'colorscheme': None
         }
-#        self.data_handler = algocli.DataHandler('python', 'insertionsort', args)
+        #self.data_handler = algocli.DataHandler('python', 'insertionsort', args)
+        #
+        #
+    def test_evenNumber(self):
+        '''Test Number is even'''
+        num = 4
+        self.assertTrue(num % 2 == 0)
+        print(self._testMethodName)
+
+    def test_oddNumber(self):
+        '''Test Number is even'''
+        num = 5
+        self.assertTrue(num % 2 != 0)
+        print(self._testMethodName)
+
+    # TODO: test different request (i.e Main page, coding page)
+    #       test replacement chars
+    #       test EVERYTHING!
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
